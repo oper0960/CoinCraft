af38254f72dbb66f12e070c80b67a95692475d18
Gilwan Ryu <opop_gwhi@naver.com>
2020-02-25 11:07:30 +0900

    - Repository API Call Rx Change

--- CoinCraft/CoinCraft/RIBs/Coin/Repository/CoinRepository.swift
+++ CoinCraft/CoinCraft/RIBs/Coin/Repository/CoinRepository.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 protocol CoinRepository {
     func getCoinMarketCapList(complete: @escaping ([CoinViewModel]) -> (Void))
@@ -30,32 +32,34 @@ class CoinRepositoryFactory {
 }
 
 class RemoteCoinRepository: CoinRepository {
+    
+    private var disposeBag = DisposeBag()
+    
     // MARK: - CoinMarketCap List
     func getCoinMarketCapList(complete: @escaping ([CoinViewModel]) -> (Void)) {
+        
         NetworkService.request(method: .get,
                                reqURL: Constants.Coin.CoinMarketCap.list,
                                parameters: DictionaryType(),
                                headers: HeadersType())
-        { (coins: [Coin]) in
-            var coinViewModels = [CoinViewModel]()
-            for coin in coins {
-                coinViewModels.append(CoinViewModel(coin: coin))
-            }
-            complete(coinViewModels)
-        }
+            .subscribe(onNext: { (coins: [Coin]) in
+                var coinViewModels = [CoinViewModel]()
+                for coin in coins {
+                    coinViewModels.append(CoinViewModel(coin: coin))
+                }
+                complete(coinViewModels)
+            }).disposed(by: disposeBag)
     }
     
     // MARK: - CryptoCompareCoin List
     func getCryptoCompareList(progressBar: @escaping (Double) -> (Void), complete: @escaping (CryptoCompareResponse) -> (Void)) {
-        NetworkService.progressRequest(method: .get,
-                                       reqURL: Constants.Coin.CryptoCompare.list,
-                                       parameters: DictionaryType(),
-                                       headers: HeadersType(),
-                                       progressBar: { progress in
-                                        progressBar(progress)})
-        { (list: CryptoCompareResponse) in
-            complete(list)
-        }
+        NetworkService.request(method: .get,
+                               reqURL: Constants.Coin.CryptoCompare.list,
+                               parameters: DictionaryType(),
+                               headers: HeadersType())
+            .subscribe(onNext: { (list: CryptoCompareResponse) in
+                complete(list)
+            }).disposed(by: disposeBag)
     }
     
     // MARK: - CryptoCompareCoin Detail
@@ -68,28 +72,39 @@ class RemoteCoinRepository: CoinRepository {
                                reqURL: Constants.Coin.CryptoCompare.detail,
                                parameters: paramaters,
                                headers: HeadersType())
-        { (coin: CryptoCompareDetailResponse) in
-            complete(coin.detail!)
-        }
+            .subscribe(onNext: { (coin: CryptoCompareDetailResponse) in
+                complete(coin.detail!)
+            }).disposed(by: disposeBag)
     }
 }
 
 // MARK: - Only Local Network (Not Usable)
 class LocalCoinRepository: CoinRepository {
-    func getCryptoCompareList(progressBar: @escaping (Double) -> (Void), complete: @escaping (CryptoCompareResponse) -> (Void)) {
-        NetworkService.progressRequest(method: .get,
-                                       reqURL: Constants.Coin.CryptoCompare.list,
-                                       parameters: DictionaryType(),
-                                       headers: HeadersType(),
-                                       progressBar: { progress in
-                                        progressBar(progress)})
-        { (list: CryptoCompareResponse) in
-            complete(list)
-        }
+    
+    private var disposeBag = DisposeBag()
+    
+    func getCoinMarketCapList(complete: @escaping ([CoinViewModel]) -> (Void)) {
+    NetworkService.request(method: .get,
+                                 reqURL: Constants.Coin.CoinMarketCap.list,
+                                 parameters: DictionaryType(),
+                                 headers: HeadersType())
+              .subscribe(onNext: { (coins: [Coin]) in
+                  var coinViewModels = [CoinViewModel]()
+                  for coin in coins {
+                      coinViewModels.append(CoinViewModel(coin: coin))
+                  }
+                  complete(coinViewModels)
+              }).disposed(by: disposeBag)
     }
     
-    func getCoinMarketCapList(complete: ([CoinViewModel]) -> (Void)) {
-        complete([CoinViewModel]())
+    func getCryptoCompareList(progressBar: @escaping (Double) -> (Void), complete: @escaping (CryptoCompareResponse) -> (Void)) {
+        NetworkService.request(method: .get,
+                           reqURL: Constants.Coin.CryptoCompare.list,
+                           parameters: DictionaryType(),
+                           headers: HeadersType())
+        .subscribe(onNext: { (list: CryptoCompareResponse) in
+            complete(list)
+        }).disposed(by: disposeBag)
     }
     
     func getCompareCoinDetail(id: String, complete: @escaping (CompareCoinDetail) -> (Void)) {
@@ -101,11 +116,9 @@ class LocalCoinRepository: CoinRepository {
                                reqURL: Constants.Coin.CryptoCompare.detail,
                                parameters: paramaters,
                                headers: HeadersType())
-        { (coin: CryptoCompareDetailResponse) in
-            
-            
-            complete(coin.detail!)
-        }
+            .subscribe(onNext: { (coin: CryptoCompareDetailResponse) in
+                complete(coin.detail!)
+            }).disposed(by: disposeBag)
     }
 }
 
